0afb82c0b99de6a053a7a3fdd9b164fbd3ec129b
もじょんぬ <moe.e.coop@gmail.com>
2019-02-18 23:19:45 +0900

    Update deploy setting

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo $MOECOOP_DOMAIN > dist/CNAME
+# echo $MOECOOP_DOMAIN > dist/CNAME
 
 cd dist
 
@@ -9,4 +9,4 @@ git config user.name 'Travis CI'
 git config user.email "$MOECOOP_MAIL"
 git add -A
 git commit -m "Deploy to Github Pages"
-git push -f -q "https://${GH_TOKEN}@github.com/${MOECOOP_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push -f -q "https://${GH_TOKEN}@github.com/${MOECOOP_REPO}.git" sandbox:gh-pages > /dev/null 2>&1
