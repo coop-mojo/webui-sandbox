68371504aee135e5584721770cb8c020e9bb56a8
もじょんぬ <moe.e.coop@gmail.com>
2019-02-20 02:24:24 +0900

    ボトムナビの配置などを改善

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,38 @@
 <template>
   <v-app dark>
-    <v-system-bar status>
-        <v-img
-          :src="require('./assets/logo.svg')"
-          class="my-3"
-          contain
-          height="18"
-        ></v-img>
-        <span>生協の知恵袋 (冗長なので削除)</span>
-        <v-spacer></v-spacer>
-    </v-system-bar>
-
-    <v-content fluid>
+    <v-content>
       <HelloWorld/>
-    <v-bottom-nav absolute>
-     <v-btn
-        flat
-        value="recent"
-      >
-        <span>アイテム検索</span>
-     </v-btn>
-     <v-btn
-        flat
-        value="recent"
-      >
-        <span>バインダー</span>
-     </v-btn>
-     <v-btn
-        flat
-        value="recent"
-      >
-        <span>レシピ一括</span>
-     </v-btn>
-     <v-btn
-        flat
-        value="recent"
-      >
-        <span>設定</span>
-     </v-btn>
-    </v-bottom-nav>
+      <v-bottom-nav
+        absolute
+        :active.sync="nav"
+        :value="true"
+        >
+        <v-btn
+          flat
+          value="item"
+          >
+          <span>アイテム検索</span>
+        </v-btn>
+        <v-btn
+          flat
+          value="binder"
+          >
+          <span>バインダー</span>
+        </v-btn>
+        <v-btn
+          flat
+          value="recipes"
+          >
+          <span>レシピ一括</span>
+        </v-btn>
+        <v-btn
+          flat
+          value="config"
+          >
+          <span>設定</span>
+        </v-btn>
+      </v-bottom-nav>
     </v-content>
-
   </v-app>
 </template>
 
@@ -54,7 +46,7 @@ export default {
   },
   data () {
     return {
-      //
+      nav: 'item'
     }
   }
 }
